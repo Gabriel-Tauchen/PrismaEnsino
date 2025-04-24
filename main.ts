f1081afa027b2639f47b7fb7b5d003e683b2345a
Gabriel <tauchen.gabriel@gmail.com>
2025-04-24 15:06:32 -0300

    main+Create

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-console.log("Hello, world!");
+import { PrismaClient} from './generated/prisma'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // const poder = await prisma.poder.create({
+    //     data: {
+    //         nome: 'Eletricidade instantânea',
+    //         descricao: 'concentra raios elétricos',
+    //     },
+    // })
+    const ser = await prisma.ser.create({
+        data: {
+            nome: 'Loki',
+            sexo: 'Masculino',
+            morto: false,
+            tipo: 'DIVINDADE',
+        },
+    })
+    const poder = await prisma.poder.create({
+        data: {
+            nome: 'Telepatia',
+            descricao: 'Manipular pensamentos',
+        },
+    })
+       
+    const artefato = await prisma.artefato.create({
+        data: {
+            nome: 'Coroa Psíquica',
+            objeto: 'Coroa',
+            material: 'Cristal Mental',
+            destruido: false,
+            nomeSer: ser.nome,
+        },
+    })
+    const relacao = await prisma.artefatoPoder.create({
+        data: {
+            artefatoNome: artefato.nome,
+            poderId: poder.idPoder,
+    },
+    })
+       
+}
+
+main()
